f71fff580c2e037f66520abd902b85b97a941159
Mario Garcia Sutil <mariosutil2003@gmail.com>
2023-10-06 10:31:23 +0200

    ImportModel creation (in progress)

---
+++ Mishima Engine/Game/Source/ImportModel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Source/External/Assimp/include/cimport.h"
+#include "../Source/External/Assimp/include/scene.h"
+#include "../Source/External/Assimp/include/postprocess.h"
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
+namespace Modelimporter {
+
+
+
+
+
+}
+
