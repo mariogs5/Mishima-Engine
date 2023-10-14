71ae893b1ec32b141f387d2a4f92117b51cc0da7
Mario Garcia Sutil <mariosutil2003@gmail.com>
2023-10-14 17:59:23 +0200

    Class Mesh implementation (incomplete)

---
+++ Mishima Engine/Game/Source/Mesh.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../Source/External/Assimp/include/cimport.h"
+#include "../Source/External/Assimp/include/scene.h"
+#include "../Source/External/Assimp/include/postprocess.h"
+//#pragma comment (lib, "..Source/External/Assimp/libx86/assimp.lib")
+
+#include <iostream>
+#include "vector"
+
+//#include <gl/GL.h>
+#include "External/MathGeoLib/include/Math/float3.h"
+#include "../Source/External/Glew/include/glew.h"
+
+struct Vertex {
+    float3 Position;
+    float3 Normal;
+    float3 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;  
+};
+
+class Mesh {
+public:
+    // mesh data
+    std::vector<Vertex>       vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture>      textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    //void Draw(Shader& shader);
+
+        //  render data
+    unsigned int VAO, VBO, EBO;
+
+    void setupMesh();
+private:
+
+
+    
+};
