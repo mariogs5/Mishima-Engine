f44c360c4d7e162d030af19cf3a6bb3f4a95c21b
Mario Garcia Sutil <mariosutil2003@gmail.com>
2023-10-16 10:02:53 +0200

    Mesh.cpp & Model.cpp (incomplete)

--- Mishima Engine/Game/Source/Mesh.h
+++ Mishima Engine/Game/Source/Mesh.h
@@ -8,7 +8,6 @@
 #include <iostream>
 #include "vector"
 
-//#include <gl/GL.h>
 #include "External/MathGeoLib/include/Math/float3.h"
 #include "../Source/External/Glew/include/glew.h"
 
@@ -30,7 +29,7 @@ public:
     std::vector<unsigned int> indices;
     std::vector<Texture>      textures;
 
-    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
     {
         this->vertices = vertices;
         this->indices = indices;
@@ -46,32 +45,6 @@ private:
     //  render data
     unsigned int VAO, VBO, EBO;
 
-    void setupMesh()
-    {
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
-            &indices[0], GL_STATIC_DRAW);
-
-        // vertex positions
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // vertex texture coords
-        glEnableVertexAttribArray(2);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
-
-        glBindVertexArray(0);
-    }
+    void setupMesh();
 };
 
